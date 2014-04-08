ca8bb2581a07e8b146fbe6c8c14966445c8178f5
Dan Parnham <dan@emergent-design.co.uk>
2014-04-08 13:40:01 +0100

    Implemented a reset event helper that can act as either a Manual or AutoResetEvent.

---
+++ include/emergent/thread/ResetEvent.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <mutex>
+#include <condition_variable>
+
+
+/// Helper class that is the equivalent to both a ManualResetEvent and
+/// AutoResetEvent.
+class ResetEvent
+{
+	public:
+
+		/// Sets the state of the event, allowing a single thread to proceed.
+		void Set();
+
+		/// Resets the state of the event, a thread will block at the wait function.
+		void Reset();
+
+		/// Wait for the event, if it has already been set then the function will
+		/// drop straight through, otherwise it will block until either Set is invoked
+		/// or a timeout occurs.
+		/// If "timeout" is greater than 0 the function will timeout if Set is not called
+		/// in time and the function will return false.
+		/// If "reset" is enabled then the flag will reset to false when Wait finishes (like
+		/// an AutoResetEvent) otherwise the flag will be left alone (like a ManualResetEvent).
+		bool Wait(int timeout = 0, bool reset = true);
+
+	private:
+
+		/// Stores the state of the event
+		bool flag = false;
+
+		/// The mutex used to protect this event
+		std::mutex cs;
+
+		/// Used to notify the Wait call so that it can unblock
+		std::condition_variable condition;
+};
