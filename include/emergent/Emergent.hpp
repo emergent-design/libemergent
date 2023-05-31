c74b41fe43034d34a024a4ed848e433ef0d47c6e
Dan Parnham <dan@emergent-design.co.uk>
2023-05-31 15:15:39 +0100

    Fix issue with C++20 is_contiguous check - uses the concept instead

--- include/emergent/Emergent.hpp
+++ include/emergent/Emergent.hpp
@@ -15,17 +15,14 @@ namespace emergent
 	typedef std::function<bool(int)> event;
 
 	// Check that a container is a contiguous buffer. Be aware that random_aceess_iterator_tag doesn't
-	// actually guarantee a contiguous buffer but in c++20 we can use contiguous_iterator_tag
-	template <typename T> inline constexpr bool is_contiguous = std::is_base_of<
-		#if __cplusplus > 201703L
-			std::contiguous_iterator_tag,
-		#else
-			std::random_access_iterator_tag,
-		#endif
-		typename std::iterator_traits<
-			decltype(std::begin(std::declval<T const>()))
-		>::iterator_category
-	>::value;
+	// actually guarantee a contiguous buffer but in c++20 we can use the contiguous_iterator concept
+	#if __cplusplus > 201703L
+		template <typename T> inline constexpr bool is_contiguous = std::contiguous_iterator<typename T::iterator>;
+	#else
+		template <typename T> inline constexpr bool is_contiguous = std::is_base_of<std::random_access_iterator_tag,
+			typename std::iterator_traits<decltype(std::begin(std::declval<T const>()))>::iterator_category
+		>::value;
+	#endif
 }
 
 /// Shortened form of the namespace
